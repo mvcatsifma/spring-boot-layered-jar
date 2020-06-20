22f7280fe470f30b49dec1bac1dcb5013d15c06e
Thomas Delnoij <info@dsen.nl>
2020-06-20 11:43:44 +0200

    Initial import

---
+++ src/main/java/nl/dsen/sblayered/SbLayeredApplication.java
@@ -0,0 +1,13 @@
+package nl.dsen.sblayered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbLayeredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbLayeredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/dsen/sblayered/SbLayeredApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.dsen.sblayered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbLayeredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
